f62bc4d9e0c562ba097c5097069bf9219c62dc01
Chris Chun <christopherchun@brandeis.edu>
2025-12-09 17:32:58 -0500

    minor notification update

--- project/fitness-tracker/app/(tabs)/session.tsx
+++ project/fitness-tracker/app/(tabs)/session.tsx
@@ -15,7 +15,7 @@ export default function SessionScreen() {
   };
 
   const handleTimerComplete = () => {
-    Alert.alert('Rest Completed');
+    Alert.alert('Timer Complete!');
   };
 
   if (!currentWorkout) {
